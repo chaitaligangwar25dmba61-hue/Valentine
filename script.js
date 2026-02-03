d40e8f19d24c83aaee1503e85a64eb6616faf27d
chaitaligangwar25dmba61-hue <chaitaligangwar_25dmba61@dtu.ac.in>
2026-02-04 00:14:26 +0530

    Rename envelope variable to container

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Elements
-const envelope = document.getElementById("envelope-container");
+const container = document.getElementById("messi-container");
 const letter = document.getElementById("letter-container");
 const noBtn = document.querySelector(".no-btn");
 const yesBtn = document.querySelector(".btn[alt='Yes']");
